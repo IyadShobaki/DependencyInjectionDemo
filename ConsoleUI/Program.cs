f36f4995a6a9e76e5b07bb88f040966223d146e8
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-01 00:08:17 -0400

    Initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BusinessLogic businessLogic = new BusinessLogic();
+            //businessLogic.ProcessData();
+
+            var container = ContainerConfig.Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
